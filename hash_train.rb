cbf9f44af730d79a2435958ce1ed3b9267ba2137
YOUR_GITHUB_USER_NAME <YOUR_GITHUB_EMAIL_ADDRESS>
2020-12-20 15:33:55 -0500

    Done.

---
+++ hash_train.rb
@@ -0,0 +1,5 @@
+
+nba_roster = {"Cleveland Cavaliers" => ["Lebron James", 
+"Kevin Love", "Kyrie Irwing"], "Golden State Worriors" => ["Stephen Curry", "Klay Thompson", "Kevin Durant"]}
+
+p nba_roster["Cleveland Cavaliers"]
